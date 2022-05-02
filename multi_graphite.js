2dd1f0644fd9365d8ae31f9cd42ed86ffe260e02
Ivan Bessarabov <ivan@bessarabov.ru>
2022-05-02 17:53:17 +0300

    Using JSX

--- multi_graphite.js
+++ multi_graphite.js
@@ -13,10 +13,10 @@ class MultiGraphite extends React.Component {
       return 'You liked this.';
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        Like
+      </button>
     );
   }
 }
